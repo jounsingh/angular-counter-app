80183286ea3ba87e6ff981fd9df02ba9be49af77
jounsingh <jounpratapsingh9@gmail.com>
2022-08-09 17:35:37 +0530

    project completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title:string = 'Angular Counter App';
+
+  count:number = 0;
+
+  handleIncrease = () => {
+    this.count += 1;
+  }
+
+  handleDecrease = () => {
+    this.count -= 1;
+  }
+
+  handleReset = () => {
+    this.count = 0;
+  }
+
 }
